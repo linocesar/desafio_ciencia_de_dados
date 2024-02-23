56b2846753e44adb3db4669068af61ba9fff233e
VERT-Lino César de Sousa Pereira <79623409249@pcivil.rj.gov.br>
2024-02-23 17:42:34 -0300

    Add concat.py para concaternr os .csv presentes no diretório merged e salvar resultado grupo|.csv e ubgrupo.csv no diretório concat

---
+++ datasus/web/concat.py
@@ -0,0 +1,53 @@
+import os
+import re
+import pandas as pd
+
+
+def create_directory(base: str, directory: str) -> str:
+    path = os.path.join(base, directory)
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"Directory {path} created")
+        return f"Directory {path} created"
+    return path
+
+
+def concat_grupo(merged_dir: str, concat_dir: str):
+    padrao_grupo = r'^grupo_.*$'
+    grupo = []
+    arquivos = (
+                [arquivo for arquivo in os.listdir(merged_dir)
+                 if arquivo.endswith('.csv') and
+                 re.match(padrao_grupo, arquivo)]
+    )
+    for arquivo in arquivos:
+        csv = os.path.join(merged_dir, arquivo)
+        df = pd.read_csv(csv, sep=',', encoding='utf-8')
+        grupo.append(df)
+    df_concat = pd.concat(grupo, ignore_index=True)
+    df_concat.to_csv(os.path.join(concat_dir, 'grupo.csv'), index=False)
+
+
+def concat_subgrupo(merged_dir: str, concat_dir: str):
+    padrao_subgrupo = r'^subgrupo_.*$'
+    subgrupo = []
+    arquivos = (
+                [arquivo for arquivo in os.listdir(merged_dir)
+                 if arquivo.endswith('.csv') and
+                 re.match(padrao_subgrupo, arquivo)]
+    )
+    for arquivo in arquivos:
+        csv = os.path.join(merged_dir, arquivo)
+        df = pd.read_csv(csv, sep=',', encoding='utf-8')
+        subgrupo.append(df)
+    df_concat = pd.concat(subgrupo, ignore_index=True)
+    df_concat.to_csv(os.path.join(concat_dir, 'subgrupo.csv'), index=False)
+
+
+if __name__ == "__main__":
+    path_base = os.path.dirname(os.path.abspath(__file__))
+    storage = os.path.join(path_base, 'storage')
+    merged = os.path.join(storage, 'merged')
+    concat = create_directory(storage, 'concat')
+    concat_grupo(merged, concat)
+    concat_subgrupo(merged, concat)
