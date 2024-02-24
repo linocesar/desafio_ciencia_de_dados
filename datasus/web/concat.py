ae6a3a38bd1102be726cbf7f772e623c7df59994
linocesar <souzaces@gmail.com>
2024-02-24 19:57:03 -0300

    Tratando exception com diretório nao existente

--- datasus/web/concat.py
+++ datasus/web/concat.py
@@ -4,26 +4,26 @@ import pandas as pd
 
 
 def create_directory(base: str, directory: str) -> str:
-    """Cria um diretório se ele não existir
+    """Cria um diretorio se ele nao existir
 
-    :param base: Caminho base
-    :param directory: Nome do diretório
-    :return: Caminho do diretório criado
+    :param str base:
+    :param str directory: Nome do diretorio
+    :return: Caminho do diretorio criado
+    :rtype: str
     """
     path = os.path.join(base, directory)
     if not os.path.exists(path):
         os.makedirs(path)
         print(f"Directory {path} created")
-        return f"Directory {path} created"
+        return path
     return path
 
 
 def concat_grupo(merged_dir: str, concat_dir: str):
     """Concatena os arquivos do grupo
 
-    :param merged_dir: Diretório dos arquivos mesclados
-    :param concat_dir: Diretório dos arquivos concatenados
-    :return:
+    :param str merged_dir: Diretorio dos arquivos mesclados
+    :param str concat_dir: Diretorio dos arquivos concatenados
     """
     padrao_grupo = r'^grupo_.*$'
     grupo = []
@@ -37,14 +37,16 @@ def concat_grupo(merged_dir: str, concat_dir: str):
         df = pd.read_csv(csv, sep=',', encoding='utf-8')
         grupo.append(df)
     df_concat = pd.concat(grupo, ignore_index=True)
-    df_concat.to_csv(os.path.join(concat_dir, 'grupo_procedimento.csv'), index=False, encoding='utf-8')
-
+    try:
+        df_concat.to_csv(os.path.join(concat_dir, 'grupo_procedimento.csv'), index=False, encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Directory {concat_dir} not found")
 
 def concat_subgrupo(merged_dir: str, concat_dir: str):
     """Concatena os arquivos do subgrupo procedimento
 
-    :param str merged_dir: Diretório dos arquivos mesclados
-    :param str concat_dir: Diretório dos arquivos concatenados
+    :param str merged_dir: Diretorio dos arquivos mesclados
+    :param str concat_dir: Diretorio dos arquivos concatenados
     """
     padrao_subgrupo = r'^subgrupo_.*$'
     subgrupo = []
@@ -58,7 +60,10 @@ def concat_subgrupo(merged_dir: str, concat_dir: str):
         df = pd.read_csv(csv, sep=',', encoding='utf-8')
         subgrupo.append(df)
     df_concat = pd.concat(subgrupo, ignore_index=True)
-    df_concat.to_csv(os.path.join(concat_dir, 'subgrupo_procedimento.csv'), index=False, encoding='utf-8')
+    try:
+        df_concat.to_csv(os.path.join(concat_dir, 'subgrupo_procedimento.csv'), index=False, encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Directory {concat_dir} not found")
 
 
 if __name__ == "__main__":
@@ -70,3 +75,4 @@ if __name__ == "__main__":
     concat_grupo(merged, concat)
     print("Concatenando subgrupo dataset..")
     concat_subgrupo(merged, concat)
+    print("Concatenado!")
