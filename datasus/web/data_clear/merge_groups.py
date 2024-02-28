e3ea3e249c96a943983d1c50f8a60bddb08ae546
linocesar <souzaces@gmail.com>
2024-02-27 22:13:36 -0300

    add ordenaçao alfabética aos arquivos do grupo

--- datasus/web/data_clear/merge_groups.py
+++ datasus/web/data_clear/merge_groups.py
@@ -25,7 +25,8 @@ def merge_grupo_subgrupo(grupo: dict, diretorio_input: str, diretorio_merged: st
             nome_arquivo = 'grupo_subgrupo_procedimento_quantidade_valor_aprovado_' + mes_ano + '.csv'
 
             if len(grupo[mes_ano]) == 2:
-
+                # Ordena os arquivos .csv do grupo alfabeticamente
+                grupo[mes_ano] = sorted(grupo[mes_ano])
                 # Arquivo csv grupo procedimento quantidade a ser lido
                 csv_quantidade = get_path_filename(diretorio_input, grupo[mes_ano][0])  # os.path.join(diretorio_output, grupo[mes_ano][0])
                 # Arquivo csv grupo procedimento valor a ser lido
@@ -35,8 +36,8 @@ def merge_grupo_subgrupo(grupo: dict, diretorio_input: str, diretorio_merged: st
                 # Dataframe grupo procedimento valor a ser lido
                 df_valor = pd.read_csv(csv_valor, sep=',', encoding='utf-8')
                 # Dataframe grupo procedimento quantidade valor a ser mesclado
-                df_merged = pd.merge(df_quantidade, df_valor, left_on='cod_municipio', right_on='cod_municipio',
-                                     how='inner', suffixes=('_grupo', '_subgrupo'))
+                df_merged = pd.merge(df_quantidade, df_valor, on=['cod_municipio', 'uf', 'municipio', 'ano', 'mes'],
+                                     suffixes=('_grupo', '_subgrupo'))
                 # Arquivo csv mesclado a ser salvo
                 path_filename = get_path_filename(diretorio_merged, nome_arquivo)
                 df_merged.to_csv(path_filename, sep=',', index=False, encoding='utf-8')
